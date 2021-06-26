27a3f68f155f0bdcc7a7281c2c24ead4ef2a4e46
Quebinck <quebinck@protonmail.com>
2021-06-27 00:33:25 +0200

    Start of header

---
+++ src/Header/Header.js
@@ -0,0 +1,18 @@
+import "./Header.css";
+import HomeIcon from "@material-ui/icons/Home";
+import IconButton from "@material-ui/core/IconButton";
+
+function Header() {
+  return (
+    <header>
+      <div id="logo">
+        <IconButton>
+          <HomeIcon style={{ fontSize: 30, color: "#219ebc" }} />
+        </IconButton>
+        <h1 id="logo-name">Naepho</h1>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
